9172a3bc12d52df2c8f39aba0d83feef30f57f0c
brayan <brayan.valdebenito@gmail.com>
2020-03-13 16:16:54 -0300

    arreglando devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   resources :posts
   get 'home/index'
   devise_for :users
-  root 'home#index'
   resources :products do
-  post :get_barcode, on: :collection
+    post :get_barcode, on: :collection
+    root 'home#index'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
